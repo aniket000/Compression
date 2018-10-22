a4f995fbba74b49948809007dd192b44bef94dfe
aniket000 <36483457+aniket000@users.noreply.github.com>
2018-10-23 00:14:42 +0530

    Second Commit
    Updated

--- main.c
+++ main.c
@@ -1,38 +1,47 @@
-#include<stdio.h>
-#include<string.h>
-
-char *encoding(char *str)
-{
-    int i;
-    char scr;
-    int len = strlen(str);
-    for(i=0;i<=len;i++)
-    {
-        int count = 0;
-        while(str[i]==str[i+1])
-        {
-            count++;
-            i++;
-            scr = str[i];
-        }
-        if(count>1)
-        {
-            str[i] = count;
-            str[i+1] =scr;
-        }
-        else
-            str[i] = scr;
-    }
-
-return str;
-}
+#include <stdio.h>
+#include <limits.h>
+#include <errno.h>
 
 int main()
 {
-    char str;
-    printf("Enter the string to encode\n");
-    scanf("%c",&str);
-    encoding(str);
-    printf("Encoded string : \n",&str);
-return 0;
+    FILE *inFile;
+    FILE *outFile;
+    char file_name[100];
+    int result;
+    int mode;
+
+    //options for project
+    //  printf("Enter the full path of the file: \n")
+    // scanf("%s",&file_name);
+
+    inFile = fopen("test.txt","r");
+    outFile = fopen("test.txt","w");
+
+    if (inFile == NULL || outFile == NULL)
+            {
+              printf("Error! Could not open file\n");
+              exit(-1); // must include stdlib.h
+            }
+    printf("What you want to do\n");
+    printf("1.Encode\n2.Decode");
+    scanf("%d",&mode);
+    /* we have valid parameters encode or decode */
+    switch (mode)
+    {
+        case 1:
+            result = RleEncodeFile(inFile, outFile);
+            break;
+
+        case 2:
+            result = RleDecodeFile(inFile, outFile);
+            break;
+
+        default:
+            fprintf(stderr, "Illegal encoding/decoding option\n");
+            result = EINVAL;
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+    return result;
 }
